2f64790c7820192f3d067ab19e2c801d5049c1f2
Anhad Jai Singh <ffledgling@gmail.com>
2014-06-17 23:41:50 -0700

    Fixed tools.py permissions error

--- tools.py
+++ tools.py
@@ -15,12 +15,16 @@ class ToolManager(object):
         """ Folder specifies the tools in a given folder
             Verification file is a CSV of <MD5,FILE NAME> pairs
         """
-        logging.info('Setting up Tool Manager')
+        logging.info('Setting up Tool Manager in folder %s on channel %s' % (folder, channel))
         self.folder = os.path.abspath(folder)
         with open(os.path.abspath(verification_file), 'r') as f:
             self.verification_list = dict(l.split(',')[::-1] for l in f.read().split('\n') if l)
         os.path.isdir(self.folder)
         self.channel = channel
+        self.mar = os.path.join(self.folder, 'mar')
+        self.mbsdiff = os.path.join(self.folder, 'mbsdiff')
+        self.unwrap = os.path.join(self.folder, 'unwrap_full_update.pl')
+        self.make_incremental = os.path.join(self.folder, 'make_incremental_update.sh')
 
     def check_integrity(self):
         # There is probably a better/more consistent way to do this as well
@@ -57,13 +61,14 @@ class ToolManager(object):
         else:
             raise oddity.ToolError('Could not determine platform')
         mar_tools_url = "http://ftp.mozilla.org/pub/mozilla.org/firefox/%s/latest-mozilla-central/mar-tools/%s/" % (self.channel, platform_version)
+        logging.info('downloading tools from %s' % mar_tools_url)
         subprocess.call([os.path.abspath('download-tools.sh'), '-o', self.folder])
-        mar = os.path.join(self.folder, 'mar')
-        mbsdiff = os.path.join(self.folder, 'mbsdiff')
-        subprocess.call(['wget', '-O', mar, mar_tools_url+'mar'])
-        subprocess.call(['wget', '-O', mbsdiff, mar_tools_url+'mbsdiff'])
-        os.chmod(mar, os.stat(mar).st_mode | stat.S_IEXEC)
-        os.chmod(mbsdiff, os.stat(mbsdiff).st_mode | stat.S_IEXEC)
+        subprocess.call(['wget', '-O', self.mar, mar_tools_url+'mar'])
+        subprocess.call(['wget', '-O', self.mbsdiff, mar_tools_url+'mbsdiff'])
+        os.chmod(self.mar, os.stat(self.mar).st_mode | stat.S_IEXEC)
+        os.chmod(self.mbsdiff, os.stat(self.mbsdiff).st_mode | stat.S_IEXEC)
+        os.chmod(self.unwrap, os.stat(self.unwrap).st_mode | stat.S_IEXEC)
+        os.chmod(self.make_incremental, os.stat(self.make_incremental).st_mode | stat.S_IEXEC)
 
     def get_path(self):
         if not self.check_integrity():
