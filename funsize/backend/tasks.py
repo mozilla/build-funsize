92d2b610f254bcd9c787ba73ecca2ecf7e00c273
Mihai Tabara <tabara.mihai@gmail.com>
2014-09-17 17:54:21 -0700

    Celery tasks to use celery specific logger instead of global one.

--- funsize/backend/tasks.py
+++ funsize/backend/tasks.py
@@ -7,20 +7,21 @@ This module contains a wrapper for the celery tasks that are to be run
 """
 
 import time
-import logging
 from celery import Celery
-
+from celery.utils.log import get_task_logger
 import funsize.backend.core as core
 
 app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
+
+logger = get_task_logger(__name__)
 
 
 @app.task
 def build_partial_mar(*args):
     """ Wrapper for actual method, to get timestamps and measurings
     """
-    logging.info('STARTING TASK')
+    logger.info('STARTING TASK')
     start_time = time.time()
     core.build_partial_mar(*args)
     total_time = time.time() - start_time
-    logging.info("Backup TOTAL TIME: %s", divmod(total_time, 60))
+    logger.info("Backup TOTAL TIME: %s", divmod(total_time, 60))
