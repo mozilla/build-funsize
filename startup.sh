c14df8b421d3f532417e21593c83c4b543ffcbc9
Mihai Tabara <tabara.mihai@gmail.com>
2014-09-28 15:45:39 -0700

    Remove startup.sh file.

--- startup.sh
+++
@@ -1,8 +0,0 @@
-set -e
-echo "Starting RabbitMQ"
-rabbitmq-server &
-echo AMQ pid: $!
-echo "Starting celery as a daemon"
-celery worker --detach -f /usr/local/var/log/celeryworker.log -l DEBUG -A senbonzakura.backend.tasks
-echo "Starting flask"
-python senbonzakura/frontend/api.py
