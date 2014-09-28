8cc5b9640e6890f0df9144653ebe2aee8bcc5c18
Mihai Tabara <tabara.mihai@gmail.com>
2014-09-27 23:42:50 -0700

    Update old project naming senbonzakura to funsize in docler_init.sh.

--- docker_init.sh
+++ docker_init.sh
@@ -3,7 +3,7 @@ echo "Starting RabbitMQ"
 rabbitmq-server -detached
 
 echo "Starting celery as a daemon"
-celery worker --detach -f /var/log/celerylog.log -l DEBUG -A senbonzakura.backend.tasks
+celery worker --detach -f /var/log/celerylog.log -l DEBUG -A funsize.backend.tasks
 
 echo "Starting flask"
-python senbonzakura/frontend/api.py
+python funsize/frontend/api.py
