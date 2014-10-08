6d4dcce321370f628b80a199081d7c9612f15fa3
Mihai Tabara <tabara.mihai@gmail.com>
2014-10-08 12:02:08 -0700

    Replace paths sending with sha512 + file in funsize_common.sh.

--- client/update-packaging/funsize_common.sh
+++ client/update-packaging/funsize_common.sh
@@ -4,43 +4,48 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #
-# This tool generates incremental update packages for the update system.
+# This tool contains functions that are to be used to handle/enable funsize
 # Author: Mihai Tabara
+#
 
 DEFAULT_URL='http://127.0.0.1:5000/cache'
 
+getsha512(){
+    echo "$(openssl sha512 "${1}" | awk '{print $2}')"
+}
+
 upload_patch(){
-    path_from="$1"
-    path_to="$2"
+    sha_from=`getsha512 "$1"`
+    sha_to=`getsha512 "$2"`
     path_patch="$3"
 
-    cmd="curl -sSw %{http_code} -o /dev/null -X POST $DEFAULT_URL -d path_from=$path_from&path_to=$path_to&path_patch=$path_patch"
+    cmd="curl -sSw %{http_code} -o /dev/null -X POST $DEFAULT_URL -F sha_from="$sha_from" -F sha_to="$sha_to" -F patch_file="@$path_patch""
     ret_code=`$cmd`
 
     if [ $ret_code -eq 200 ]
     then
-        echo "$path_patch successful uploaded to funsize!"
+        echo ""$path_patch" Successful uploaded to funsize!"
         return 0;
     fi
 
-    echo "$path_patch failed to be uploaded to funsize!"
+    echo ""$path_patch" Failed to be uploaded to funsize!"
     return 1;
 }
 
 get_patch(){
-    path_from="$1"
-    path_to="$2"
-    path_patch="$3"
+    sha_from=`getsha512 "$1"`
+    sha_to=`getsha512 "$2"`
+    destination_file="$3"
 
-    cmd="curl -sSGw %{http_code} $DEFAULT_URL -o $path_patch --data-urlencode "path_from=$path_from" --data-urlencode "path_to=$path_to""
+    cmd="curl -sSGw %{http_code} $DEFAULT_URL -o "$destination_file" --data-urlencode "sha_from=$sha_from" --data-urlencode "sha_to=$sha_to""
     ret_code=`$cmd`
 
     if [ $ret_code -eq 200 ]
     then
-        echo "successful retrieved $path_patch from funsize!"
+        echo "Successful retrieved $destination_file from funsize!"
         return 0;
     fi
 
-    echo "failed to retrieve $path_patch from funsize!"
+    echo "Failed to retrieve $destination_file from funsize!"
     return 1;
 }
