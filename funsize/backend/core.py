696005795fe161f563be640f25896b6a1e3fb596
Mihai Tabara <tabara.mihai@gmail.com>
2014-10-20 14:42:18 -0700

    Move local cache dir env var as part of MBSDIFF_HOOK new tool.

--- funsize/backend/core.py
+++ funsize/backend/core.py
@@ -105,7 +105,6 @@ def generate_partial_mar(cmar_new, cmar_old, difftools_path, channel_id,
     my_env['MOZ_PRODUCT_VERSION'] = product_version
     my_env['LC_ALL'] = 'C'
     my_env['MBSDIFF_HOOK'] = '/vagrant/src/client/update-packaging/funsize_common.sh -A http://127.0.0.1:5000/cache'
-    my_env['FUNSIZE_LOCAL_CACHE_DIR'] = '/tmp'
 
     try:
         os.mkdir(working_dir)
