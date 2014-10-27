732b7d9d3c13ca490a614624c14b33c1ae1386c9
Mihai Tabara <tabara.mihai@gmail.com>
2014-10-27 11:37:34 -0700

    Logging done right in funsize_client.py

--- client/funsize_client.py
+++ client/funsize_client.py
@@ -9,22 +9,31 @@ This module contains a client side to call for partials the funsize server
 import argparse
 import requests
 import time
+import logging
+import sys
+
 try:
     import simplejson as json
 except ImportError:
     import json
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
-FUNSIZE_SERVER_SIDE = 'http://127.0.0.1:5000'
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+FUNSIZE_SERVER_SIDE = 'http://funsize-env.elasticbeanstalk.com'
 
 
 class FunsizeClient(object):
     """ Class to hold funsize client methods """
-
-    lifecycles = 20 # number of lifecycles to keep polling for results
-    cycle_sleep = 30 # number of seconds to sleep in a cycle
+    lifecycles = 20  # number of lifecycles to keep polling for results
+    cycle_sleep = 30  # number of seconds to sleep in a cycle
 
     def __init__(self):
+        logger.info("Funsize client successfully initiated.")
         pass
 
     def _write_to_file(self, ret, output_file):
@@ -34,20 +43,20 @@ class FunsizeClient(object):
 
     def demand_partial(self, args):
         """ Method to contain th general logic of the module """
-
         trigger_resource_uri = FUNSIZE_SERVER_SIDE + '/partial'
+        logger.info("Triggering job at %s", trigger_resource_uri)
         ret = self.trigger_partial_request(trigger_resource_uri, args)
 
         if ret.status_code == 500:
             raise SystemError('Encoutered error on funsize server side - 500')
 
+        logger.info("Server returned. Investigating response ...")
         _uri = json.loads(ret.content).get('result', None)
         getter_resource_uri = FUNSIZE_SERVER_SIDE + _uri
         self.poll_for_partial(getter_resource_uri, args.output)
 
     def trigger_partial_request(self, resource_uri, iargs):
         """ Method used to request partial from funsize server side """
-
         payload = {
             'mar_from': iargs.from_url,
             'mar_to': iargs.to_url,
@@ -61,15 +70,17 @@ class FunsizeClient(object):
 
     def poll_for_partial(self, resource_uri, output_file):
         """ Method to retrieve partial in max  lifecycles * cycle_sleep time """
-
+        logger.info("Polling for the partial at %s", resource_uri)
         ret = requests.get(resource_uri)
         if ret.status_code == 400:
             raise ValueError("Bad identifier request!")
 
+        logger.info("Start querying for the partial resource ...")
         counter = self.lifecycles
         while counter:
             ret = requests.get(resource_uri)
             if ret.status_code == 200:
+                logger.info("Partial resource retrieved, writing to file ...")
                 self._write_to_file(ret, output_file)
                 break
             counter -= 1
