86365f5e30aef3f3fcf6987b42ba6c1cf9b9ed67
Mihai Tabara <tabara.mihai@gmail.com>
2014-10-30 15:32:28 -0700

    Bugfix in core.py

--- funsize/backend/core.py
+++ funsize/backend/core.py
@@ -34,7 +34,7 @@ def get_complete_mar(url, mar_hash, output_file=None):
     """ Return binary string if no output_file specified """
     logging.info('Downloading complete MAR %s with mar_hash %s', url, mar_hash)
 
-    if url.ststartswith('http://') or url.ststartswith('https://'):
+    if url.startswith('http://') or url.startswith('https://'):
         mar = fetch.downloadmar(url, mar_hash, output_file=output_file)
     else:
         cacheo = cache.Cache()
